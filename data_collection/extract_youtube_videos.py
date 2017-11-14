adaa0e924b9de1f149562758c1279c5471968e2f
Vidhi Jain <vidhi.musu@gmail.com>
2017-11-14 23:56:19 +0530

    Removing redundant code

--- data_collection/extract_youtube_videos.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-import youtube_dl
-
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
-
-# import subprocess
-# subprocess.call(['youtube-dl','-o',output_path+'/%(title)s.(ext)s',"--extract-audio","--audio-format","mp3",decoded_url])
